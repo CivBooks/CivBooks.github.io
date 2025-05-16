758dbe89148b12c126217f2ae4f5ad208b0635c5
VastInfiniteNet <screwthisusernameprocess@gmail.com>
2025-05-15 23:23:15 -0400

    - dynamically add a .stendhal download button to each book .html page

--- book.js
+++ book.js
@@ -1,1 +1,37 @@
 // This will provide library work utilities in the future.
+
+function saveAsStendhalFile() {
+	let stendhalText =  `title: ${document.querySelector('[property="og:title"]').content}\n`
+	                +   `author: ${document.querySelector('[class="signee-name"]').innerText}\n`
+	                +   "pages:\n";
+	Array.from(document.getElementsByClassName("page-content")).forEach(page => {
+		stendhalText += `#- ${page.textContent}\n`;
+	});
+	stendhalText += "#- ";
+
+	// output to file, download, and cleanup
+	const element = document.createElement('a');
+	element.href = window.URL.createObjectURL(new Blob([stendhalText]));
+	// use page title as filename. Consider using `book title-author-iteration` instead  
+	element.download = `${document.getElementsByTagName("title")[0].innerText}.stendhal`;
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
+
+function insertStendhalDownloadButton() {
+    var buttonNode = document.createElement('button');
+    buttonNode.type = "button";
+    buttonNode.onclick = () => saveAsStendhalFile();
+    buttonNode.innerText = "Download as .Stendhal";
+
+    var stendhalDownloadNode = document.createElement('div');
+    stendhalDownloadNode.classList.add('download-button');
+    stendhalDownloadNode.appendChild(buttonNode);
+
+    let homeNode = document.getElementsByClassName("back-home")[0];
+    homeNode.parentNode.insertBefore(stendhalDownloadNode, homeNode.nextSibling);
+}
+
+insertStendhalDownloadButton();
